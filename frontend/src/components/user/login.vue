1a83468d09ca9a646cf56580788ae6c18f6091fd
KyungUp Won <jack@netsarang.com>
2022-01-11 18:00:01 +0900

    push login page

---
+++ frontend/src/components/user/login.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="inspire">
+    <v-main class="blue-grey lighten-4">
+      <!-- Login Component -->
+      <v-container style="max-width: 450px" fill-height>
+        <v-layout align-center row wrap>
+          <v-flex xs12>
+            <v-card>
+              <div class="pa-10">
+                <h1 style="text-align: center" class="mb-10">Login</h1>
+                <form>
+                  <v-text-field
+                    label="ID"
+                    prepend-inner-icon="mdi-account"
+                  ></v-text-field>
+                  <v-text-field
+                    prepend-inner-icon="mdi-lock"
+                    type="password"
+                    label="Password"
+                  >
+                  </v-text-field>
+                  <v-btn
+                    type="submit"
+                    color="blue lighten-1 text-capitalize"
+                    depressed
+                    large
+                    block
+                    dark
+                    class="mb-3"
+                    @click="onLogin"
+                  >
+                    Login
+                  </v-btn>
+                  <v-btn
+                    @click="onSignup"
+                    color="blue lighten-1 text-capitalize"
+                    depressed
+                    large
+                    block
+                    dark
+                  >
+                    Sign Up
+                  </v-btn>
+                </form>
+              </div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data: () => ({
+    }),
+    methods: {
+      onLogin () {
+        console.log('onLogin')
+      },
+      onSignup() {
+        this.$router.push('/login/signup')
+      }
+    }
+  }
+</script>
